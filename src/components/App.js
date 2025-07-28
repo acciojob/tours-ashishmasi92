75fb1692beca90ea119792deea5efaed10b376c6
ashish <ashishmasih92g@gmail.com>
2025-07-28 20:38:25 +0530

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "../../tour.json"
+import "../styles/App.css"
+console.log(data);
 
 const App = () => {
-    return(
-      <main id="main">
-      </main>
-    )
+  let [tourList, setTourList] = useState(data)
+  let [selected, setSelected] = useState(null)
+
+  function selected_div(id) {
+
+    setSelected(selected === id ? null : id)
+
+  }
+
+
+  
+
+
+
+  return (
+    <main id="main">
+      <h1>Tours List</h1>
+      <div className="container">
+
+        {tourList.length == 0 && <div>
+          <h1>No tours left</h1>
+          <button onClick={() => {
+            window.location.reload()
+          }} className="btn" >reload</button>
+        </div>}
+
+        {tourList.length > 0 && tourList.map((list, i) => {
+
+          return <div className="single-tour" onClick={() => {
+            selected_div(i)
+          }}>
+            <img src={list.image} />
+            <h3 className="title" >{list.name}</h3>
+            <p className="tour-price">${list.price}</p>
+            <div>
+              {selected === i ? (
+                <p className="tour-info">{list.description}</p>
+
+              ) : null}
+            </div>
+            <button onClick={() => {
+              setTourList(tourList.filter((v, index) => {
+                return index != i
+              }))
+            }} className="delete-btn">Delete</button>
+          </div>
+        })}
+
+
+      </div>
+
+
+
+    </main>
+  )
 }
 export default App;
---
+++ src/components/ToursList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import x from "tour.json"
+
+
+const Tours = () =>{
+    
+    console.log(x);
+
+    return(
+        <>
+        
+
+        
+        </>
+    )
+}
+
+export default Tours
